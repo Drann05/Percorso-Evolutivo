3a10aa49104cb3b00aaa8c5b0699599956345b18
Silviaaaaaa <silviafarina310@gmail.com>
2025-12-25 23:08:23 +0100

    Funzioni GUI e game view

--- src/views/views.py
+++ src/views/views.py
@@ -1,16 +1,26 @@
 from abc import ABC, abstractmethod
+from breezypythongui import EasyFrame, EasyCanvas
 
-class Views(ABC):
+class Views(EasyFrame, ABC):
+    """Classe base astratta per tutte le schermate dell'applicazione"""
+    def __init__(self, title, width, height):
+        EasyFrame.__init__(self, title=title, width=width, height=height)
+        self.build_ui()
 
     @abstractmethod
     def build_ui(self):
+        """Metodo astratto obbligatorio per costruire l'interfaccia"""
         pass
 
     @abstractmethod
     def update(self):
+        """Metodo astratto obbligatorio per aggiornare i widget"""
         pass
 
+    def create_label(self, container, text, row, column, sticky="NSEW"):
+        return container.addLabel(text=text, row=row, column=column, sticky=sticky)
+
     @abstractmethod
-    def handle_events(self):
+    def handle_events(self, event_type, **kwargs):
         pass
 
