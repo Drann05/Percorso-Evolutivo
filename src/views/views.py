9cbf9eabffd40e99b836db913f4caeb1e1b989cc
Silviaaaaaa <silviafarina310@gmail.com>
2025-12-26 18:57:38 +0100

    Definizione delle funzioni della classe Views

--- src/views/views.py
+++ src/views/views.py
@@ -1,16 +1,30 @@
 from abc import ABC, abstractmethod
+from breezypythongui import EasyCanvas, EasyFrame
 
-class Views(ABC):
+class Views(EasyFrame, ABC):
+    """Classe base astratta per tutte le schermate dell'applicazione"""
+    def __init__(self, title, width, height):
+        EasyFrame.__init__(self, title=title, width=width, height=height)
+        self.build_ui()
 
     @abstractmethod
     def build_ui(self):
+        """Metodo astratto obbligatorio per costruire l'interfaccia"""
         pass
 
     @abstractmethod
     def update(self):
+        """Metodo astratto obbligatorio per aggiornare i widget"""
         pass
 
     @abstractmethod
-    def handle_events(self):
+    def handle_events(self, event_type, **kwargs):
         pass
+
+    def create_label(self, container, text, row, column, sticky="NSEW"):
+        return container.addLabel(text=text, row=row, column=column, sticky=sticky)
+
+    def clear(self):
+        for widget in self.winfo_children():
+            widget.destroy()
 
