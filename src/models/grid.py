20a246a9f7f601e1b6c990f524c834159cab0e18
Drann <gioelespano@hotmail.com>
2025-12-30 23:55:27 +0100

    Aggiunge funzione cell_count() e funzione generative_dfs() alla classe Grid e modifica la funzione generate_grid

--- src/models/grid.py
+++ src/models/grid.py
@@ -20,6 +20,12 @@ class Grid:
         "Difficile": {"Muri": 25, "Risorse": 8, "Trappole": 7}
     }
 
+    MURO = 'X'
+    RISORSA = 'R'
+    TRAPPOLA = 'T'
+    OBIETTIVO = 'O'
+    PUNTO_DI_PARTENZA = 'P'
+    CELLA_VUOTA = '.'
 
     def __init__(self, width, height):
         """
@@ -31,7 +37,8 @@ class Grid:
 
         self._width = width
         self._height = height
-        self._grid = []
+        self._grid = [[Cell(col,row,self.MURO) for col in range(self._width)] for row in range(self._height)]
+        self._grid_dimension = width * height
         self._safe_zone = []
         self._spawn_point = None # Tupla (x,y)
 
@@ -48,36 +55,50 @@ class Grid:
         - Difficile: 25% Muri, 8% Risorse, 7% Trappole
         """
 
-        self._grid = [[Cell(col,row,'X') for col in range(self._width)] for row in range(self._height)]
-        grid_dimension = self.get_grid_dimension()
-        #muri_count = int(self.DIFFICULTY[difficulty]["Muri"] * grid_dimension / 100)
-        #risorse_count = int(self.DIFFICULTY[difficulty]["Risorse"] * grid_dimension / 100)
-        #trappole_count = int(self.DIFFICULTY[difficulty]["Trappole"] * grid_dimension / 100)
+        self.generative_dfs()  # Algoritmo per generare la griglia con almeno una strada percorribile
+
+        muri_count = int(self.DIFFICULTY[difficulty]["Muri"] * self._grid_dimension / 100) - self.cell_count(self.MURO)
+        risorse_count = int(self.DIFFICULTY[difficulty]["Risorse"] * self._grid_dimension / 100)
+        trappole_count = int(self.DIFFICULTY[difficulty]["Trappole"] * self._grid_dimension / 100)
 
         def place_cells(cell_type, count):
-            placed = 0
-            while placed < count:
-                row = randint(0, self._height - 1)
-                col = randint(0, self._width - 1)
-                if self._grid[col][row].get_type() == ".":
-                    self._grid[col][row] = Cell(col, row, cell_type)
-                    placed += 1
 
-        #place_cells("X", muri_count)
-        #place_cells("R", risorse_count)
-        #place_cells("T", trappole_count)
+            if count < 0:
+                removed = 0
+                while removed < abs(count):
+                    row = randint(0, self._height - 1)
+                    col = randint(0, self._width - 1)
+                    if self._grid[row][col].get_type() == cell_type:
+                        self._grid[row][col] = Cell(row, col, self.CELLA_VUOTA)
+                        removed += 1
+            else:
+                placed = 0
+                while placed < count:
+                    row = randint(0, self._height - 1)
+                    col = randint(0, self._width - 1)
+                    if self._grid[row][col].get_type() == self.CELLA_VUOTA:
+                        self._grid[row][col] = Cell(row, col, cell_type)
+                        placed += 1
 
-    def dfs(self):
+        place_cells(self.MURO, muri_count)
+        place_cells(self.RISORSA, risorse_count)
+        place_cells(self.TRAPPOLA, trappole_count)
+        place_cells(self.OBIETTIVO, 1)
+
+    def generative_dfs(self):
+        """
+        Genera un labirinto utilizzando un algoritmo Depth-First Search (DFS)
+        in versione generativa
+
+        L'algoritmo parte da una cella iniziale (inizializzata a 0,0) e visita
+        (in modo iterativo tramite stack) le celle non ancora visitate.
+        """
+
         visited = set()
-
         stack = [(0,0)]
-        is_goal_reached = False
 
-        while (not is_goal_reached) and stack:
-            x, y = stack.pop()
-
-
-            self.set_cell((x, y), '.')  # scava la cella corrente
+        while stack:
+            x, y = stack[-1]
 
             neighbors = []
             for dx, dy in [(2, 0), (-2, 0), (0, 2), (0, -2)]:  # vicini a distanza 2
@@ -86,16 +107,18 @@ class Grid:
                     if (nx, ny) not in visited:
                         neighbors.append((nx, ny))
 
-
-            random.shuffle(neighbors)  # per percorsi casuali
-
-            for nx, ny in neighbors:
+            if neighbors:
+                nx, ny = random.choice(neighbors)
                 mid_x = (x+nx)//2
                 mid_y = (y+ny)//2
 
-                self._grid[mid_x][mid_y] = Cell(mid_x, mid_y, '.')
+                self._grid[mid_x][mid_y] = Cell(mid_x, mid_y, self.CELLA_VUOTA) # Scava la cella intermedia
+                self.set_cell((x, y), self.CELLA_VUOTA)  # Scava la cella corrente
                 visited.add((nx, ny))
                 stack.append((nx, ny))
+            else:
+                stack.pop()
+
 
     def get_spawn_position(self):
         """
@@ -104,6 +127,14 @@ class Grid:
             TODO: implementare logica di spawn
         """
         return (3,2)
+
+    def cell_count(self, cell_type):
+        counter = 0
+        for row in range(self._height):
+            for col in range(self._width):
+                if self._grid[row][col].get_type() == cell_type:
+                    counter += 1
+        return counter
 
     def get_cell(self, position):
         row, col = position
@@ -128,10 +159,13 @@ class Grid:
 
 if __name__ == '__main__':
     grid = Grid(20, 20)
-    grid.generate_grid("Difficile")
 
-    grid.dfs()
+    #grid.generate_grid("Difficile")
+    grid.generative_dfs()
     grid.print_grid()
+    print(grid.cell_count('X'))
+    print(grid.cell_count('R'))
+    print(grid.cell_count('T'))
 
 
 
