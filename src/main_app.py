a1933c0d9619faef1c0db49cf4e4afa244dd59d0
Drann <gioelespano@hotmail.com>
2026-01-02 18:10:59 +0100

    Aggiunge la funzione show_start_screen()

--- src/main_app.py
+++ src/main_app.py
@@ -3,20 +3,18 @@ from .views.start_screen import StartScreen
 from .views.leaderboard_view import LeaderboardView
 from .views.game_view import GameView
 from .views.menu import Menu
-from breezypythongui import EasyFrame
+
 
 class MainApp():
     def __init__(self):
-        pass
-        #self.leaderboard = LeaderboardView(self)
-        #self.start_screen = StartScreen(self)
-        #self.game_view = GameView(self)
-        #self.menu = Menu(self)
+        self._nickname = None
+        self._difficulty = None
+        self.run()
+
 
     def run(self):
         """Funzione che avvia il programma"""
         self.show_start_screen()
-        self._widget = []
 
     def show_menu(self):
         pass
@@ -28,7 +26,10 @@ class MainApp():
         pass
 
     def show_start_screen(self):
-        pass
+        self.start_screen = StartScreen(self, "Percorso Evolutivo")
+        self.start_screen.mainloop()
+        self._nickname = self.start_screen.nickname
+        self._difficulty = self.start_screen.difficulty
 
 if __name__ == '__main__':
     app = MainApp()
