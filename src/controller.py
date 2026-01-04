c2d00d77b28a29b33e49910ec72971ddede7ad9d
Drann <gioelespano@hotmail.com>
2026-01-04 22:19:21 +0100

    Modifica il Controller in modo da renderlo coerente con la nuova classe MainView

--- src/controller.py
+++ src/controller.py
@@ -9,26 +9,36 @@ from .views.menu import Menu
 class MainApp():
     def __init__(self):
 
-        self.start_screen = None
-        self.game = None
-        self.game_view = None
+        self._start_screen = None
+        self._game = None
+        self._game_view = None
 
-        self.main_view = MainView(self)
-        self.main_view.mainloop()
+        self._main_view = MainView(self)
+        self._main_view.mainloop()
 
 
     def init_menu(self):
         pass
 
     def init_game(self):
-        pass
+        nickname = self._start_screen.nickname
+        difficulty = self._start_screen.difficulty
+        self._game = Game(nickname, difficulty)
+        self._game.start_game()
+
+        self._main_view.show_game()
 
     def init_leaderboard(self):
         pass
 
     def init_start_screen(self):
-        self.main_view.show_start_screen()
+        self._main_view.show_start_screen()
 
-    
+    def handle_movement(self, direction):
+        self._game.move_player(direction)
+        new_pos = self._game.player.position
+        self._main_view.game_view.update_player_position_display()
+        self._main_view.game_view.update_cell_display(new_pos)
+
 if __name__ == '__main__':
     app = MainApp()
