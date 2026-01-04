6b2964400c2e02df987766b3357a95b4e3c31328
Drann <gioelespano@hotmail.com>
2026-01-04 21:28:17 +0100

    Aggiunge self._parent_view per ogni widget che necessita EasyFrame

--- src/views/start_screen.py
+++ src/views/start_screen.py
@@ -6,13 +6,15 @@ class StartScreen(Views):
     ACCENT_COLOR = "Pink"
     BACKGROUND_COLOR = "Purple"
 
-    def __init__(self, parent, title, width=500, height=500):
+    def __init__(self, parent_view, controller, title, width=500, height=500):
         super().__init__(title, width, height)
         self._widgets = []
-        self._parent = parent
-        self._difficulty=None
+        self._parent_view = parent_view
+        self._controller = controller
+        self._difficulty = None
         self._nickname = None
-        self.grid_init(12,12)
+
+        self._parent_view.grid_init(12,12)
         self.build_ui()
 
 
@@ -22,31 +24,31 @@ class StartScreen(Views):
         widget["background"]=self.BACKGROUND_COLOR
 
     def build_ui(self):
-        title = self.addLabel("PERCORSO EVOLUTIVO", row=2, column=4, columnspan=2)
+        title = self._parent_view.addLabel("PERCORSO EVOLUTIVO", row=2, column=4, columnspan=2)
         title["foreground"]="white"
         title["background"]="Pink"
         title["font"]=self.MAIN_FONT
         self._widgets.append(title)
 
-        user=self.addLabel(text="Inserisci un nickname ( massimo 20 caratteri)", row = 3, column=4, columnspan=2)
+        user=self._parent_view.addLabel(text="Inserisci un nickname ( massimo 20 caratteri)", row = 3, column=4, columnspan=2)
         user["foreground"]="white"
         user["background"]=self.BACKGROUND_COLOR
         user["font"]=self.MAIN_FONT
         self._widgets.append(user)
 
-        self.nickname_field=self.addTextField(text="", row = 4, column=4, columnspan=2)
+        self.nickname_field=self._parent_view.addTextField(text="", row = 4, column=4, columnspan=2)
         self.nickname_field["font"]=self.MAIN_FONT
         self._widgets.append(self.nickname_field)
 
-        self.start_button=self.addButton(text="Start game", row=5, column=4, columnspan=2, command=self.handle_start_btn)
+        self.start_button=self._parent_view.addButton(text="Start game", row=5, column=4, columnspan=2, command=self.handle_start_btn)
         self.style(self.start_button)
         self._widgets.append(self.start_button)
 
-        self.quit_button=self.addButton(text="Quit game", row=6, column=4, columnspan=2, command=self.quit)
+        self.quit_button=self._parent_view.addButton(text="Quit game", row=6, column=4, columnspan=2, command=self._parent_view.quit)
         self.style(self.quit_button)
         self._widgets.append(self.quit_button)
 
-        self.error_label = self.addLabel(
+        self.error_label = self._parent_view.addLabel(
             text="Messaggio di errore",
             row=4,
             column=4,
@@ -58,7 +60,7 @@ class StartScreen(Views):
 
         self.error_label.grid_remove()
 
-        self.correct_label = self.addLabel(
+        self.correct_label = self._parent_view.addLabel(
             text="Messaggio",
             row=5,
             column=4,
@@ -116,7 +118,7 @@ class StartScreen(Views):
         if dialog.choice:
             self._difficulty = dialog.choice
             print(f"Hai scelto la difficoltà: {self.difficulty}")
-            self.messageBox(title="Pronto!", message=f"Partita avviata in modalità {self.difficulty}")
+            self._parent_view.messageBox(title="Pronto!", message=f"Partita avviata in modalità {self.difficulty}")
         else:
             self.error_label["text"] = "Devi scegliere una difficoltà per iniziare!"
             self.error_label.grid()
