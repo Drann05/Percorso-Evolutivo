8b4922f9aba0f0ff5806626f26c084af6fcdf734
Drann <gioelespano@hotmail.com>
2026-01-09 18:19:06 +0100

    Separa la logica del flickering dello schermo in una funzione a parte. Viene chiamata soltanto quando viene attivata un'abilità speciale

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,5 @@
-from models.leaderboard import Leaderboard
-from views.leaderboard_view import LeaderboardView
+from .models.leaderboard import Leaderboard
+from .views.leaderboard_view import LeaderboardView
 from .models.game import Game
 from .views.main_view import MainView
 
