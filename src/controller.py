fe5bc6281e9b31464c9c696aee75593674aeae43
Drann <gioelespano@hotmail.com>
2026-01-10 17:42:07 +0100

    Rende il codice più pulito senza attuare alcuna modifica alla logica

--- src/controller.py
+++ src/controller.py
@@ -17,58 +17,74 @@ class Controller:
         self.leaderboard = Leaderboard()
         self.game = None
 
+        self._session_data = {"nickname": None}
+
         self._main_view = MainView(self)
 
 
     def run(self):
         self._main_view.mainloop()
 
-    def init_menu(self):
-        raise NotImplementedError("Menu non ancora implementato")
-
-    def init_leaderboard(self):
-        raise NotImplementedError("Menu non ancora implementato")
-
+    #---------------------------------|
+    #   GESTIONE NAVIGAZIONE E MENU   |
+    #---------------------------------|
     def init_start_screen(self):
         """Imposta lo stato iniziale dell'app e mostra
         la shermata di avvio attraverso MainView"""
         self._main_view.show_start_screen()
 
-            #--------------------#
-            #    START SCREEN    #
-            # -------------------#
+    def handle_leaderboard_request(self):
+        scores = self.leaderboard.get_top_10()
+        self._main_view.show_leaderboard(scores)
 
+    def handle_instruction_request(self):
+        self._main_view.show_instructions()
 
-
-
-
-
-                #------------#
-                #    GAME    #
-                #------------#
-
+    #---------------------|
+    #   LOGICA DI GIOCO   |
+    #---------------------|
 
     def start_game_request(self, nickname):
-        self.pending_nickname = nickname
+        self._session_data["nickname"] = nickname
         self._main_view.show_difficulty_dialog()
 
     def handle_selected_difficulty(self, difficulty):
-        self.start_game(self.pending_nickname, difficulty)
-
-    def start_game(self, nickname, difficulty):
-        """Inizializza il gioco passando all'istanza gli attributi
-        nickname e difficulty, ottenuti dalla classe Start Screen"""
-
+        nickname = self._session_data["nickname"]
         self.game = Game(nickname, difficulty)
         self.game.start_game()
-        self._main_view.change_screen(self._main_view.show_game)
+        self._main_view.show_game()
+
+    def handle_movement_request(self, direction):
+        """Gestisce il movimento del giocatore e aggiorna
+        l'interfaccia grafica di GameView"""
+
+        if not self.game:
+            return
+
+        move_result = self.game.move_player(direction)
+
+        if move_result["moved"]:
+            self._main_view.update_game(self.get_game_state())
+
+            if move_result["game_over"]:
+                self.handle_game_over()
+
+    def handle_special_action_request(self, action, target_position):
+
+        if not self.game:
+            return False
+
+        is_success = self.game.use_special_action(action, target_position)
+        if is_success:
+            self._main_view.update_game(self.get_game_state())
+            return True
+
+        return False
 
     def handle_restart_game_request(self):
-        #game_config = self.game.get_game_config()
-        #del self.game
-        #self.start_game(game_config["nickname"],game_config["difficulty"])
-        self.game.restart_game()
-        self._main_view.change_screen(self._main_view.show_game)
+        if self.game:
+            self.game.restart_game()
+            self._main_view.show_game()
 
     def get_game_state(self):
         return {
@@ -89,44 +105,12 @@ class Controller:
         }
 
 
-
-    def handle_movement_request(self, direction):
-        """Gestisce il movimento del giocatore e aggiorna
-        l'interfaccia grafica di GameView"""
-
-        if not self.game:
-            return
-
-        moved = self.game.move_player(direction)
-
-        if not moved:
-            return
-
-        self._main_view.update_game(self.get_game_state())
-        if moved["game_over"]:
-            self.handle_game_over()
-
-    def handle_special_action_request(self, action, target_position):
-
-        if not self.game:
-            return False
-    
-        is_success = self.game.use_special_action(action, target_position)
-        if not is_success:
-            self._main_view.show_error()
-            return False
-    
-        self._main_view.update_game(self.get_game_state())
-        return True
-
     def update_timer(self):
         if self.game and self.game.timer:
             return self.game.timer.get_elapsed()
         return False
 
-    def handle_leaderboard_request(self):
-        scores = self.leaderboard.get_top_10()
-        self._main_view.change_screen(self._main_view.show_leaderboard, scores)
+
 
     def handle_game_over(self):
         won = True
@@ -151,7 +135,7 @@ class Controller:
                 print(f"Dati salvati per {nickname}: {score} punti.")
             except Exception as e:
                 print(f"Errore durante il salvataggio della classifica: {e}")
-        self._main_view.game_view.show_game_over(won)
+        self._main_view.show_game_over(won)
 
 if __name__ == '__main__':
     app = Controller()
